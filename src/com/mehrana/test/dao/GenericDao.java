f309c209bbf9b9dd3b618fdda898a2f1164c6bd8
Mehrana <fatemeh.safaeiiiii@gmail.com>
2024-12-13 23:29:29 +0330

    Column 'personnelId' not found /LeaveDao

--- src/com/mehrana/test/dao/GenericDao.java
+++ src/com/mehrana/test/dao/GenericDao.java
@@ -1,20 +1,34 @@
-package com.mehrana.test.dao;
+/* package com.mehrana.test.dao;
+
+import com.mehrana.test.connection.SimpleConnectionPool;
 
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-public interface GenericDao<T> {
+public abstract class GenericDao<T> {
+
+   // constructor            System.out.println();
+
+   public GenericDao() {
+       try {
+           SimpleConnectionPool connectionPool = new SimpleConnectionPool(); // create connection pool
+       } catch (SQLException e) {
+           throw new ExceptionInInitializerError("Failed to initialize com.mehrana.test.connection pool: " + e.getMessage());
+       }
+   }
 
 
-    Optional<T> insert(T entity) throws SQLException;
+   public abstract Optional<T> insert(T entity) throws SQLException;
 
-    Optional<T> findById(long id);
+//    public abstract Optional<Personnel> getById(long id) throws SQLException;
 
-    List<T> findAll();
+   public abstract Optional<T> getById(long id);
 
-    T update(T entity);
+   public abstract List<T> getAll();
 
-    void delete(Long id);
+   public abstract T update(T entity);
 
-}
+   public abstract void delete(Long id);
+
+}*/
